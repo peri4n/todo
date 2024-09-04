d0973d016021b961b0c91f48b84b224617f0bbd9
Fabian Bull <spam+github@fbull.de>
2024-09-04 08:12:03 +0200

    Add support for tags

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,26 @@ use anyhow::Result;
 use clap::{Parser, Subcommand};
 use sqlx::migrate::MigrateDatabase;
 use sqlx::types::chrono::NaiveDateTime;
-use sqlx::Sqlite;
+use sqlx::{Sqlite, Row};
 use std::path::PathBuf;
 
-#[derive(Debug, sqlx::FromRow)]
+#[derive(Debug)]
 struct Task {
     name: String,
     done: bool,
-    due: NaiveDateTime
+    due: NaiveDateTime,
+    tags: Vec<String>,
+}
+
+impl sqlx::FromRow<'_, sqlx::sqlite::SqliteRow> for Task {
+    fn from_row(row: &sqlx::sqlite::SqliteRow) -> std::result::Result<Self, sqlx::Error> {
+        Ok(Task {
+            name: row.get("name"),
+            done: row.get("done"),
+            due: NaiveDateTime::parse_from_str(row.get("due"), "%Y-%m-%d %H:%M:%S").unwrap(),
+            tags: row.get::<'_, String, &str>("tags").split(",").map(|s| s.to_string()).collect(),
+        })
+    }
 }
 
 #[derive(Parser)]
@@ -77,18 +89,33 @@ async fn main() -> Result<()> {
                     id INTEGER PRIMARY KEY, 
                     name TEXT NOT NULL, 
                     done BOOLEAN NOT NULL DEFAULT 0,
-                    due DATE)")
+                    due DATE);")
                 .execute(&pool)
                 .await?;
+
+            sqlx::query("CREATE TABLE tags (
+                        id INTEGER PRIMARY KEY, 
+                        name TEXT NOT NULL
+                    );")
+                .execute(&pool)
+                .await?;
+
+            sqlx::query("CREATE TABLE tagged (
+                task_id INTEGER,
+                tag_id INTEGER
+            );").execute(&pool).await?;
             println!("Done initializing database");
         },
         Some(Commands::List) => {
-            sqlx::query_as::<_, Task>("SELECT * FROM tasks")
+            sqlx::query_as::<_, Task>(r#"
+                SELECT tasks.*, GROUP_CONCAT(tags.name) AS tags  
+                FROM tasks LEFT JOIN tagged ON tasks.id = tagged.task_id LEFT JOIN tags ON tagged.tag_id = tags.id
+                GROUP BY tasks.id, tasks.name, tasks.due, tasks.done"#)
                 .fetch_all(&pool)
                 .await?
                 .iter()
                 .for_each(|task| {
-                    println!("Task: {} (done: {}, due: {})", task.name, task.done, task.due.format("%Y-%m-%dT%H:%M:%S"));
+                    println!("Task: {} (done: {}, due: {}, tags: {})", task.name, task.done, task.due.format("%Y-%m-%dT%H:%M:%S"), task.tags.join(", "));
                 });
             println!("Listing all tasks");
         },
