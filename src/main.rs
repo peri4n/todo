aca78994319e5b7f3420e35779a3e241b2d5a06c
Fabian Bull <spam+github@fbull.de>
2024-09-26 14:26:27 +0200

    Reformat SQL

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ pub mod cli;
 pub mod tag;
 pub mod task;
 
-use comfy_table::presets::UTF8_BORDERS_ONLY;
-use comfy_table::*;
 use anyhow::Result;
 use clap::Parser;
 use cli::*;
@@ -21,61 +19,43 @@ async fn main() -> Result<()> {
     // create a connection pool
     let pool = sqlx::sqlite::SqlitePool::connect("sqlite:tasks.db").await?;
 
-    match &cli.command {
-        Some(Commands::Add { name, due }) => {
-            let task_id = create_task(&pool, name, *due).await?;
-            println!("Task with id {} added", task_id);
-        }
-        Some(Commands::Init) => {
-            sqlx::query_file!("src/schema.sql").execute(&pool).await?;
-            println!("Done initializing database");
-        }
-        Some(Commands::List) => {
-            let mut table = Table::new();
-            table.load_preset(UTF8_BORDERS_ONLY)
-                .set_content_arrangement(ContentArrangement::Dynamic)
-                .set_width(120)
-                .set_header(vec!["ID", "Name", "Due", "Finished", "Tags"]);
-
-            fetch_tasks(&pool).await?.iter().for_each(|task: &Task| {
-                table.add_row(vec![
-                    Cell::new(task.id.to_string()),
-                    Cell::new(task.name.clone()),
-                    Cell::new(task.due.format("%Y-%m-%d %H:%M:%S").to_string()),
-                    Cell::new(
-                        task.finished
-                            .map(|d| d.format("%Y-%m-%d %H:%M:%S").to_string())
-                            .unwrap_or("".to_string()),
-                    ),
-                    Cell::new(task.tags.join(",")),
-                ]);
-            });
-
-            println!("{}", table);
-        }
-        Some(Commands::Done { id }) => {
-            finish_task(&pool, *id).await?;
-            println!("Finished task {}", id);
-        }
-        Some(Commands::Tag { id, tag }) => {
-            // check if task exists
-            if fetch_task_with_id(&pool, *id).await?.is_some() {
-                // check if the tag exists
-                if let Ok(tag_id) = fetch_tag_id_with_name(&pool, tag).await {
-                    tag_task(&pool, *id, tag_id).await?;
+    if let Some(command) = cli.command {
+        match command {
+            Commands::Add { name, due } => {
+                let task_id = create_task(&pool, &name, due).await?;
+                println!("Task with id {} added", task_id);
+            }
+            Commands::Init => {
+                sqlx::query_file!("src/schema.sql").execute(&pool).await?;
+                println!("Done initializing database");
+            }
+            Commands::List => {
+                let tasks = fetch_tasks(&pool).await?;
+                print_task_table(tasks);
+            }
+            Commands::Done { id } => {
+                finish_task(&pool, id).await?;
+                println!("Finished task {}", id);
+            }
+            Commands::Tag { id, tag } => {
+                // check if task exists
+                if fetch_task_with_id(&pool, id).await?.is_some() {
+                    // check if the tag exists
+                    if let Ok(tag_id) = fetch_tag_id_with_name(&pool, &tag).await {
+                        tag_task(&pool, id, tag_id).await?;
+                    } else {
+                        // if not present create the tag
+                        let tag_id = create_tag(&pool, &tag).await?;
+                        tag_task(&pool, id, tag_id as u32).await?;
+                    }
                 } else {
-                    // if not present create the tag
-                    let tag_id = create_tag(&pool, tag).await?;
-                    tag_task(&pool, *id, tag_id as u32).await?;
+                    // task not present
+                    return Err(anyhow::anyhow!("Task with id {} not found", id));
                 }
-            } else {
-                // task not present
-                return Err(anyhow::anyhow!("Task with id {} not found", id));
             }
         }
-        None => {
-            println!("No command provided");
-        }
+    } else {
+        println!("No command provided");
     }
 
     Ok(())
--- src/task.rs
+++ src/task.rs
@@ -1,5 +1,7 @@
 use std::fmt::Display;
 
+use comfy_table::presets::UTF8_BORDERS_ONLY;
+use comfy_table::{Cell, ContentArrangement, Table};
 use sqlx::types::chrono::NaiveDateTime;
 use sqlx::Row;
 
@@ -10,6 +12,31 @@ pub struct Task {
     pub(crate) finished: Option<NaiveDateTime>,
     pub(crate) due: NaiveDateTime,
     pub(crate) tags: Vec<String>,
+}
+
+pub fn print_task_table(tasks: Vec<Task>) {
+    let mut table = Table::new();
+    table
+        .load_preset(UTF8_BORDERS_ONLY)
+        .set_content_arrangement(ContentArrangement::Dynamic)
+        .set_width(120)
+        .set_header(vec!["ID", "Name", "Due", "Finished", "Tags"]);
+
+    tasks.iter().for_each(|task: &Task| {
+        table.add_row(vec![
+            Cell::new(task.id.to_string()),
+            Cell::new(task.name.clone()),
+            Cell::new(task.due.format("%Y-%m-%d %H:%M:%S").to_string()),
+            Cell::new(
+                task.finished
+                    .map(|d| d.format("%Y-%m-%d %H:%M:%S").to_string())
+                    .unwrap_or("".to_string()),
+            ),
+            Cell::new(task.tags.join(",")),
+        ]);
+    });
+
+    println!("{}", table);
 }
 
 impl Display for Task {
@@ -32,7 +59,8 @@ impl sqlx::FromRow<'_, sqlx::sqlite::SqliteRow> for Task {
         Ok(Task {
             id: row.get("id"),
             name: row.get("name"),
-            finished: NaiveDateTime::parse_from_str(row.get("finished_date"), "%Y-%m-%d %H:%M:%S").ok(),
+            finished: NaiveDateTime::parse_from_str(row.get("finished_date"), "%Y-%m-%d %H:%M:%S")
+                .ok(),
             due: NaiveDateTime::parse_from_str(row.get("due_date"), "%Y-%m-%d %H:%M:%S").unwrap(),
             tags: row
                 .get::<'_, String, &str>("tags")
@@ -44,22 +72,37 @@ impl sqlx::FromRow<'_, sqlx::sqlite::SqliteRow> for Task {
 }
 
 pub async fn fetch_tasks(pool: &sqlx::SqlitePool) -> Result<Vec<Task>, sqlx::Error> {
-    sqlx::query_as("
-        SELECT tasks.*, GROUP_CONCAT(tags.name) AS tags  
-        FROM tasks LEFT JOIN tagged ON tasks.id = tagged.task_id LEFT JOIN tags ON tagged.tag_id = tags.id
-        GROUP BY tasks.id, tasks.name, tasks.due_date, tasks.finished_date")
-        .fetch_all(pool)
-        .await
+    sqlx::query_as(
+        "
+        SELECT 
+            tasks.*, GROUP_CONCAT(tags.name) AS tags  
+        FROM tasks 
+            LEFT JOIN tagged ON tasks.id = tagged.task_id 
+            LEFT JOIN tags ON tagged.tag_id = tags.id
+        GROUP BY 
+            tasks.id, tasks.name, tasks.due_date, tasks.finished_date
+        ORDER BY 
+            tasks.due_date",
+    )
+    .fetch_all(pool)
+    .await
 }
 
 pub async fn fetch_task_with_id(
     pool: &sqlx::SqlitePool,
     id: u32,
 ) -> Result<Option<Task>, sqlx::Error> {
-    sqlx::query_as("SELECT * FROM tasks WHERE id = ?")
-        .bind(id)
-        .fetch_optional(pool)
-        .await
+    sqlx::query_as(
+        "
+        SELECT 
+            * 
+        FROM 
+            tasks 
+        WHERE id = ?",
+    )
+    .bind(id)
+    .fetch_optional(pool)
+    .await
 }
 
 pub async fn create_task(
@@ -67,20 +110,34 @@ pub async fn create_task(
     name: &str,
     due: NaiveDateTime,
 ) -> Result<i64, sqlx::Error> {
-    sqlx::query("INSERT INTO tasks (name, due_date) VALUES (?, ?)")
-        .bind(name)
-        .bind(due)
-        .execute(pool)
-        .await
-        .map(|result| result.last_insert_rowid())
+    sqlx::query(
+        "
+        INSERT INTO 
+            tasks (name, due_date) 
+        VALUES 
+            (?, ?)",
+    )
+    .bind(name)
+    .bind(due)
+    .execute(pool)
+    .await
+    .map(|result| result.last_insert_rowid())
 }
 
 pub async fn finish_task(
     pool: &sqlx::SqlitePool,
     id: u32,
 ) -> Result<sqlx::sqlite::SqliteQueryResult, sqlx::Error> {
-    sqlx::query("UPDATE tasks SET finished_date = CURRENT_TIMESTAMP WHERE id = ?")
-        .bind(id)
-        .execute(pool)
-        .await
+    sqlx::query(
+        "
+        UPDATE 
+            tasks 
+        SET 
+            finished_date = CURRENT_TIMESTAMP 
+        WHERE 
+            id = ?",
+    )
+    .bind(id)
+    .execute(pool)
+    .await
 }
